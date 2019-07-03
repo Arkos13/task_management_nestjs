da88b24cfed90148d6094c7db930bc8415628568
arkadiy <arkadiiozerov13@gmail.com>
2019-07-03 21:23:54 +0300

    improvement task api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,19 @@
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
-import { Task } from './task.model';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.taskService.getTasksWithFilter(filterDto);
+        }
         return this.taskService.getAllTasks();
     }
 
@@ -18,6 +23,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.taskService.createTask(createTaskDto);
     }
@@ -25,5 +31,11 @@ export class TasksController {
     @Delete('/:id')
     deleteTask(@Param('id') id: string): void {
         this.taskService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateStatusTask(@Param('id') id: string, 
+                    @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
+        return this.taskService.updateStatusTask(id, status);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as uuid from 'uuid/v1';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,8 +12,26 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksWithFilter(filterDto: GetTaskFilterDto): Task[] {
+        const {status, search} = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) || task.description.includes(search)
+            );
+        }
+        return tasks;
+    }
+
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if (!found) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -27,8 +46,15 @@ export class TasksService {
         return task;
     }
 
+    updateStatusTask(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
 }
