88e671e638c9e2e397a4faa9a937dc547885103c
arkadiy <arkadiiozerov13@gmail.com>
2019-07-07 16:45:35 +0300

    setting cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server')
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: process.env.SERVER_ORIGIN });
+  }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
